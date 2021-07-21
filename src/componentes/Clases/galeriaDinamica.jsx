32c7cbcd9b81f1a070ae7b5d012b99a6cf96105b
Samuel Berrus <samuelberrus@gmail.com>
2021-07-21 17:33:58 +0200

    Añadida galeria dinamica

--- src/componentes/Clases/galeriaDinamica.jsx
+++ src/componentes/Clases/galeriaDinamica.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import "./CustomStyles/galeriadinamica.css";
 
 const galeriaDinamica = () => {
+    const addImages = (images) => {
+        for (let i = 0; i < images.length; i++) {
+            const reader = new FileReader();
+            reader.readAsDataURL(images[i]);
+            reader.addEventListener("load", (e) => {
+                const newImg = `<img src="${e.currentTarget.result}">`;
+                document.querySelector("#contenedorGaleria").innerHTML += newImg;
+            });
+        }
+    };
+
     return (
         <div className="mt-5">
-            <h3>Práctica 1 - Galeria Dinámica</h3>
+            <h3>
+                Práctica 1 - Galeria Dinámica{" "}
+                <a
+                    href={`https://github.com/sberrus/masterJS/blob/master/src/componentes/Clases/galeriaDinamica.jsx`}
+                    rel="noreferrer"
+                    target="_BLANK"
+                >
+                    <i className="bi bi-github float-end text-white fs-40"></i>
+                </a>
+            </h3>
+            <input
+                type="file"
+                multiple
+                onChange={(e) => {
+                    console.log(e.target.files);
+                    addImages(e.target.files);
+                }}
+            />
+            <div id="contenedorGaleria"></div>
         </div>
     );
 };
